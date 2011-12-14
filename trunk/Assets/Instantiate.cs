2e0fca633b6b89ecdd23792c530b9450bcd0ded0
glerup9@hotmail.com <glerup9@hotmail.com@355bc658-ef80-8bcc-f180-4e97078a0761>
2011-12-14 11:15:01 +0000

    test med multiplayer/implementering

--- trunk/Assets/Instantiate.cs
+++ trunk/Assets/Instantiate.cs
@@ -5,9 +5,10 @@ public class Instantiate : MonoBehaviour
 {
 	
 	public Transform gamePlayer;
-	public Transform house;
-	public Transform bigTree;
-	public Transform text;
+//	public Transform house;
+//	public Transform bigTree;
+//	public Transform text;
+	public Vector3 location = new Vector3(23.6671f,3f,57.98985f);
 	
 	// Use this for initialization
 	void Start () 
@@ -25,9 +26,9 @@ public class Instantiate : MonoBehaviour
 	void OnNetworkLoadedLevel() 
 	{
  		// Instantiating cube when Network is loaded
-		TextMesh t = (TextMesh)text.GetComponent(typeof(TextMesh));
-		t.text = "" + Network.player.ipAddress + "";
- 		Network.Instantiate(gamePlayer, transform.position, transform.rotation, 0);
+//		TextMesh t = (TextMesh)text.GetComponent(typeof(TextMesh));
+//		t.text = "" + Network.player.ipAddress + "";
+ 		Network.Instantiate(gamePlayer, location, transform.rotation, 0);
 //		Network.Instantiate(house, transform.position, transform.rotation, 0);
 //		Network.Instantiate(bigTree, transform.position, transform.rotation, 0);
 //		Network.Instantiate(text, location1, transform.rotation, 0);
