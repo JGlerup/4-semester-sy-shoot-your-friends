e41a0df817abbdffb93be80e1e72712697821b53
anitalykkeclemmensen@gmail.com <anitalykkeclemmensen@gmail.com@355bc658-ef80-8bcc-f180-4e97078a0761>
2012-01-04 09:22:23 +0000

    BackUp

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -21,6 +21,7 @@ public class Control : MonoBehaviour
 		if(GUI.Button(new Rect(20, 100, 50,50), "Start"))
 		{
 			GameObject.Find("Player(Clone)").transform.position = new Vector3(25,2,60);
+			GameObject.Find("PlayerName(Clone)").transform.position = new Vector3(25,2,60);
 		}
 	}
 }
