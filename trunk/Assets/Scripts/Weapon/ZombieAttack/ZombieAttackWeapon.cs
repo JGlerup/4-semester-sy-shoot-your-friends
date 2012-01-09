48ffa97adfd45c7799309c8ee8d21d996f52f07a
erik.gravesen <erik.gravesen@355bc658-ef80-8bcc-f180-4e97078a0761>
2012-01-09 12:15:28 +0000

    Weapons for zombies implemented

---
+++ trunk/Assets/Scripts/Weapon/ZombieAttack/ZombieAttackWeapon.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZombieAttackWeapon : MonoBehaviour {
+    public float reloadTime = 0.5f;
+    public float lastShot = -10.0f;
+    public int damage = 6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Fire()
+    {
+        networkView.RPC("Attack", RPCMode.All, null);
+    }
+
+    [RPC]
+    void Attack()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hit, 2.0f))
+        {
+            if (Time.time > reloadTime + lastShot)
+            {
+                //hit.collider.SendMessageUpwards("ApplyDamage", hit.transform.position, SendMessageOptions.DontRequireReceiver);
+                hit.collider.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+                lastShot = Time.time;
+
+            }
+        }
+    }
+}
