b18b6d86cfa239ef6cf244f501a7217928e654bf
anitalykkeclemmensen@gmail.com <anitalykkeclemmensen@gmail.com@355bc658-ef80-8bcc-f180-4e97078a0761>
2012-01-09 09:22:04 +0000

    Win

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Win : MonoBehaviour {
+public class Win : MonoBehaviour 
+{
 	
 	public int winningTeamNumber;
+	public List<GameObject> allPlayers = new List<GameObject>();
+	public List<GameObject> allZombies = new List<GameObject>();
+	private CharacterController cc;
+	private MouseLook ml;
+	private ThirdPersonPlayer tpp;
 	
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		winningTeamNumber = 0;
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if(winningTeamNumber == 0)
 		{
 			
@@ -23,7 +31,7 @@ public class Win : MonoBehaviour {
 		{
 			if(winningTeamNumber == 1)
 			{
-				//Win1();
+				Win1();
 			}
 			if(winningTeamNumber == 2)
 			{
@@ -41,5 +49,44 @@ public class Win : MonoBehaviour {
 	
 	}
 	
+	void Win1()
+	{
+		allPlayers.AddRange(GameObject.FindGameObjectsWithTag("team1"));
+		DisablePlayers();	
+		Wait();
+	}
 	
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(10);
+	}
+	
+	void DisablePlayers()
+	{
+		allZombies.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+				
+		foreach(GameObject g in allZombies)
+		{
+			g.transform.Rotate(0,180,0);
+			g.animation.Play("idle");
+			cc = (CharacterController)g.GetComponent(typeof(CharacterController));
+			cc.enabled = false;
+			ml = (MouseLook)g.GetComponent(typeof(MouseLook));
+			ml.enabled = false;
+			tpp = (ThirdPersonPlayer)g.GetComponent(typeof(ThirdPersonPlayer));
+			tpp.enabled = false;
+		}
+		
+		foreach(GameObject g in allPlayers)
+		{
+			g.transform.Rotate(0,180,0);
+			g.animation.Play("dance");
+			cc = (CharacterController)g.GetComponent(typeof(CharacterController));
+			cc.enabled = false;
+			ml = (MouseLook)g.GetComponent(typeof(MouseLook));
+			ml.enabled = false;
+			tpp = (ThirdPersonPlayer)g.GetComponent(typeof(ThirdPersonPlayer));
+			tpp.enabled = false;
+		}
+	}
 }
